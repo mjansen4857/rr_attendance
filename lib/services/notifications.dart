2809f507aaf03902e79316d2b7fc54b8e249da3c
Michael Jansen <mjansen4857@gmail.com>
2022-08-18 14:47:58 -0400

    dumb

--- lib/services/notifications.dart
+++ lib/services/notifications.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class Notifications {
@@ -5,11 +7,28 @@ class Notifications {
       FlutterLocalNotificationsPlugin();
 
   static Future<bool?> init() async {
-    return _notifications.initialize(
+    await _notifications.initialize(
       InitializationSettings(
         android: AndroidInitializationSettings('ic_launcher'),
         iOS: IOSInitializationSettings(),
       ),
     );
+
+    if (Platform.isAndroid) {
+      return _notifications
+          .resolvePlatformSpecificImplementation<
+              AndroidFlutterLocalNotificationsPlugin>()
+          ?.requestPermission();
+    } else if (Platform.isIOS) {
+      return _notifications
+          .resolvePlatformSpecificImplementation<
+              IOSFlutterLocalNotificationsPlugin>()
+          ?.requestPermissions(
+            alert: true,
+            badge: true,
+            sound: true,
+          );
+    }
+    return false;
   }
 }
